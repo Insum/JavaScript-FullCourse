a2c7368d7330cc359f6d0c1805d2753fa0727b18
Eldar Terzic <eldar.terzic@insum.se>
2023-01-07 20:30:12 +0100

    counter

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-document.getElementById("count-el").innerText = 5
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.innerHTML = count;
+}
+
+
+function save() {
+    let countStr = count + " - "
+    saveEl.innerHTML += countStr;
+    count = 0;
+    countEl.innerHTML = 0;
+}
